1b246e8d41d0bf62bafd0e9e0588094f0662f01f
julietteceb16 <126595208+discovery23@users.noreply.github.com>
2023-09-13 16:22:10 -0600

    cambios2

---
+++ nocion/src/components/Nocion.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import { TextField } from "@mui/material";
+
+function Nocion(){
+   
+
+
+    return (
+        <h1>"hola"</h1>
+
+          
+    );
+
+}
+export default Nocion;
